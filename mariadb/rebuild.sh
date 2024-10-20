1e53afe5d75300cf5d44027bffd843cfb5191b96
ZachGregory <123396117+ZachGregory@users.noreply.github.com>
2024-10-20 03:10:41 -0400

    Update rebuild.sh

--- mariadb/rebuild.sh
+++ mariadb/rebuild.sh
@@ -5,4 +5,4 @@ docker build -t spooky-scav/$NAME . --no-cache
 docker stop $NAME
 docker rm $NAME
 
-docker run -it -d --name $NAME --net host -v $(pwd)/backups:/var/backups --mount source=db,target=/var/lib/mysql/ -e MARIADB_ROOT_PASSWORD="" --restart unless-stopped spooky-scav/$NAME --wait_timeout=31536000 --max_connections=100
+docker run -it -d --name $NAME --net host -v $(pwd)/backups:/var/backups --mount source=db,target=/var/lib/mysql/ -e MARIADB_ROOT_PASSWORD="password" --restart unless-stopped spooky-scav/$NAME --wait_timeout=31536000 --max_connections=100
